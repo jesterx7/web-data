01cf67490320a293c22dda47cb78794ac9fef8f0
jesterx7 <40884899+jesterx7@users.noreply.github.com>
2021-05-17 19:47:58 +0700

    #89ta8z[complete] search functional

--- resources/views/components/form/filter-form.blade.php
+++ resources/views/components/form/filter-form.blade.php
@@ -4,22 +4,25 @@
         Add
         </a>
     </div>
-    <div class="col-md-3">
-        <div class="form-group select--search">
-            <select name="option" class="form-control">
-            @foreach($filters as $key => $filter)
-                <option class="filter--option" value="{{ $key }}">{{ $filter }}</option>
-            @endforeach
-            </select>
+    <form class="col-md-10" action="{{ route('page.search', $page) }}" method="get" role="search">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-group select--search">
+                    <select name="option" class="form-control">
+                    @foreach($filters as $key => $filter)
+                        <option class="filter--option" value="{{ $filter['relation_name'] }}:{{ $key }}">{{ $filter['name'] }}</option>
+                    @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group select--search">
+                    <input type="text" name="search" class="form-control" placeholder="Search" required>
+                </div>
+            </div>
+            <div class="col-md-1">
+                <button type="submit" class="btn btn-info btn-fill add-table"><i class="nc-icon nc-zoom-split"></i></button>
+            </div>
         </div>
-    </div>
-    <div class="col-md-2">
-        <div class="form-group select--search">
-            <input type="text" name="search" class="form-control" placeholder="Search" required>
-        </div>
-    </div>
-    <div class="col-md-1">
-        <a class="btn btn-info btn-fill add-table" href="{{ route('page.add', $page) }}"><i class="nc-icon nc-zoom-split"></i>
-        </a>
-    </div>
+    </form>
 </div>
