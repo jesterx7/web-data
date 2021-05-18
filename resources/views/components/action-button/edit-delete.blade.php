5b681acb7d704848f18e02c849d8e7389ff2e80b
jesterx7 <40884899+jesterx7@users.noreply.github.com>
2021-05-18 14:03:07 +0700

    #8j1kqh[complete] button replacement

--- resources/views/components/action-button/edit-delete.blade.php
+++ resources/views/components/action-button/edit-delete.blade.php
@@ -1,6 +1,11 @@
 @if (Auth::user()->isAdmin())
     <div class="edit-delete--container">
         <div class="edit-delete--item">
+            <a href="{{ route('page.edit', [$page, $id]) }}" class="btn btn-info btn-fill btn-action--edit">
+                <i class="nc-icon nc-share-66"></i>
+            </a>
+        </div>
+        <div class="edit-delete--item">
             <form method="post" action="{{ route('api.delete', [$page, $id]) }}">
                 @csrf
                 @method('DELETE')
@@ -8,11 +13,6 @@
                     <i class="nc-icon nc-scissors"></i>
                 </button>
             </form>
-        </div>
-        <div class="edit-delete--item">
-            <a href="{{ route('page.edit', [$page, $id]) }}" class="btn btn-info btn-fill btn-action--edit">
-                <i class="nc-icon nc-share-66"></i>
-            </a>
         </div>
     </div>
 @endif
